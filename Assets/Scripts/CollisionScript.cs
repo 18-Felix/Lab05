f439207605b1a3df29aa1b518fd196d0a263e5c4
18-Felix <91874612+18-Felix@users.noreply.github.com>
2021-10-12 15:46:21 +0800

    Update 3
    Fixed Collision
    + Score UI & Time UI

---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollisionScript : MonoBehaviour
+{
+    public int score;
+    public Text Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score.text = "Score: " + score;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "coins")
+        {
+            score += 10;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "water")
+        {
+            SceneManager.LoadScene("Lose");
+        }
+    }
+
+}
+
